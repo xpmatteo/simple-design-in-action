7172ef0211e70b6e3be755c9d29c1e9b394ed53f
Matteo Vaccari <mvaccari@thoughtworks.com>
2015-06-15 10:28:03 +0200

    Test: introduco un difetto per Heroku

--- src/main/java/it/xpug/todolists/main/Main.java
+++ src/main/java/it/xpug/todolists/main/Main.java
@@ -11,10 +11,11 @@ import it.xpug.toolkit.web.*;
 
 public class Main {
 	public static void main(String[] args) {
-		String port = System.getenv("PORT");
-		if (port == null || port.isEmpty()) {
-			port = "8080";
-		}
+//		String port = System.getenv("PORT");
+//		if (port == null || port.isEmpty()) {
+//			port = "8080";
+//		}
+		String port = "8080";
 
 		List<TodoList> todoLists = synchronizedList(new ArrayList<TodoList>());
 		ReusableJettyApp app = new ReusableJettyApp(new TodoListsServlet(todoLists));
