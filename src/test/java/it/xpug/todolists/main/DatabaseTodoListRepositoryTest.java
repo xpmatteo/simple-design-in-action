ebb1603e0746576d8c74ff7fb7ba107ee65b1194
Matteo Vaccari <mvaccari@thoughtworks.com>
2015-06-22 08:55:07 +0200

    Add test for get all lists

--- src/test/java/it/xpug/todolists/main/DatabaseTodoListRepositoryTest.java
+++ src/test/java/it/xpug/todolists/main/DatabaseTodoListRepositoryTest.java
@@ -1,6 +1,9 @@
 package it.xpug.todolists.main;
 
 import static org.junit.Assert.*;
+
+import java.util.*;
+
 import it.xpug.toolkit.db.*;
 
 import org.junit.*;
@@ -41,4 +44,18 @@ public class DatabaseTodoListRepositoryTest {
     public void notFound() throws Exception {
 	    assertNull("not found", repository.get(9786987));
     }
+
+	@Test
+	public void allLists() throws Exception {
+	    int id0 = repository.add(new TodoList("zero"));
+	    int id1 = repository.add(new TodoList("one"));
+
+	    List<TodoList> allLists = repository.getAll();
+
+	    assertEquals(2, allLists.size());
+	    assertEquals("zero", allLists.get(0).getName());
+	    assertEquals(id0, allLists.get(0).getId());
+	    assertEquals("one", allLists.get(1).getName());
+	    assertEquals(id1, allLists.get(1).getId());
+	}
 }
