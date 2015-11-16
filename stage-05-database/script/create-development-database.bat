48cb220dc9a45cecb4f10a144e2a58c1c55bbdaf
Matteo Vaccari <mvaccari@thoughtworks.com>
2015-11-16 22:09:37 +0100

    Add windows batch files

---
+++ stage-05-database/script/create-development-database.bat
@@ -0,0 +1,6 @@
+@echo off
+
+java -cp lib/h2*.jar org.h2.tools.RunScript -url jdbc:h2:woodys-mart -script src/main/sql/000_init_schema_info.sql
+java -cp lib/h2*.jar org.h2.tools.RunScript -url jdbc:h2:woodys-mart -script src/main/sql/001_create_orders.sql
+
+
